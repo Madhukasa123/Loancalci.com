bb713fb02f79b6a3dda54ef129ad59e1e6ded91f
Madhukasa123 <116111821+Madhukasa123@users.noreply.github.com>
2023-08-05 11:40:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ function calculateLoan() {
 
     const resultDiv = document.getElementById("result");
     resultDiv.innerHTML = `
-        Number of Months: ${numMonths}<be><br>
-        Number of Days: ${numDays}<br><br>
+        Number of Months: ${numMonths}<br><br>
+        Number of Days: ${numDays}<br><br><hr>
         Interest: ${totalInterest.toFixed(2)}<br><br>
-        Total Amount: ${totalAmount.toFixed(2)}
+        Total Amount: ${totalAmount.toFixed(2)} <br><br>
     `;
 }
